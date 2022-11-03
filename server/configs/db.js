cdaf8d7bb56c6ad8f5bbb28e8b4312f567f44731
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:56:08 +0530

    db deploy on atlas

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const dbID = process.env.DB_ID;
 const dbPassword = process.env.DB_PASSWORD;
 
-const connection = mongoose.connect(
+ const connection = mongoose.connect(
  `mongodb+srv://${dbID}:${dbPassword}@cluster0.6lt3yah.mongodb.net/?retryWrites=true&w=majority`
 );
 
