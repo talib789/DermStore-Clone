c4f3b7e2eeda09b312444258d73a490910cbabbd
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:04:17 +0530

    Front End

--- client/src/Redux/action.js
+++ client/src/Redux/action.js
@@ -1,14 +1,14 @@
 import { COUNT_CART, GET_CARTDATA } from "./actionType";
 
 export const fetchCartData = (data) => {
-  return {
+   return {
     type: GET_CARTDATA,
     payload: data,
   };
 };
 
 export const GetCartCount = (data) => {
-  return {
+   return {
     type: COUNT_CART,
     payload: data,
   };
