d32a53d2a49353fc86dad3e595ed38d5fa4f603a
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:22:38 +0530

    Front End

--- client/src/Pages/Home/HomeCarosel.jsx
+++ client/src/Pages/Home/HomeCarosel.jsx
@@ -11,7 +11,7 @@ export const HomeCarosel = () => {
           className="d-block w-100"
           src="https://static.thcdn.com/images/xlarge/webp/widgets/208-us/48/original-0826-STDCRE-40745-DS-EF-Sept-Batch-SHOT33-HomePage_1920x700-063848.jpg"
           alt="First slide"
-        />
+         />
         <Carousel.Caption className="CrouselText">
           <h1 className="h1carosal">Introducing Chantecaille</h1>
           <p className="h1carosal">
@@ -24,7 +24,7 @@ export const HomeCarosel = () => {
           className="d-block w-100"
           src="https://static.thcdn.com/images/xlarge/webp/widgets/208-us/26/original-Chantecaille_Banner_1920x700_%281%29-042426.png"
           alt="Third slide"
-        />
+         />
         <Carousel.Caption className="CrouselText">
           <h1 className="h1carosal">New From SkinCeuticals</h1>
           <p className="h1carosal">
@@ -37,7 +37,7 @@ export const HomeCarosel = () => {
           className="d-block w-100"
           src="https://static.thcdn.com/images/xlarge/webp/widgets/208-us/52/original-BoD_Necessities_Kit_1920x700-083952.png"
           alt="Third slide"
-        />
+         />
         <Carousel.Caption className="CrouselText">
           <h1 className="h1carosal"> SkinCeuticals Nisl Consectetur</h1>
           <p className="h1carosal">
@@ -50,7 +50,7 @@ export const HomeCarosel = () => {
           className="d-block w-100"
           src="https://static.thcdn.com/images/xlarge/webp/widgets/208-us/26/original-Chantecaille_Banner_1920x700_%281%29-042426.png"
           alt="Third slide"
-        />
+         />
         <Carousel.Caption className="CrouselText">
           <h1 className="h1carosal"> Praesent cursus magna</h1>
           <p className="h1carosal">
