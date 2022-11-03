3f885fc72bf62e95777f1005d99e1cf7a2a4f80f
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:24:15 +0530

    Front End

--- client/src/Pages/Home/ShopByCategory.jsx
+++ client/src/Pages/Home/ShopByCategory.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const ShopByCategory = () => {
   return (
-    <>
+     <>
       <Link to="/products">
         <img
           src="https://static.thcdn.com/images/small/webp/widgets/208-us/20/DS_Round_Nav_-_Untitled_Page_%287%29-052520.png"
@@ -21,19 +21,19 @@ export const ShopByCategory = () => {
         <img
           src="https://static.thcdn.com/images/small/webp/widgets/208-us/32/DS_Round_Nav_-_Untitled_Page_%289%29-052632.png"
           alt="prod"
-        />
+         />
       </Link>
       <Link to="/products">
         <img
           src="https://static.thcdn.com/images/small/webp/widgets/208-us/03/DS_Round_Nav_-_Untitled_Page_%2810%29-052703.png"
           alt="prod"
-        />
+         />
       </Link>
       <Link to="/products">
         <img
           src="https://static.thcdn.com/images/small/webp/widgets/208-us/49/DS_Round_Nav_-_Untitled_Page_%2813%29-053649.png"
           alt="prod"
-        />
+         />
       </Link>
       <Link to="/products">
         <img
