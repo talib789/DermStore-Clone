0ba4726308116d5c92410c57f9b8f7285fa87623
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 08:23:33 +0530

    db debloy on atlas

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const dbID = process.env.DB_ID;
 const dbPassword = process.env.DB_PASSWORD;
 
 const connection = mongoose.connect(
-  `mongodb+srv://${dbID}:${dbPassword}@cluster0.6lt3yah.mongodb.net/?retryWrites=true&w=majority`
+ `mongodb+srv://${dbID}:${dbPassword}@cluster0.6lt3yah.mongodb.net/?retryWrites=true&w=majority`
 );
 
 module.exports = connection;
