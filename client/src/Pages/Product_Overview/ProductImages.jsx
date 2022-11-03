f4d97cc06af12e7e8c3f7012d453526cf3004390
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:07:06 +0530

    Front End

--- client/src/Pages/Product_Overview/ProductImages.jsx
+++ client/src/Pages/Product_Overview/ProductImages.jsx
@@ -6,16 +6,16 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 export const ProductImages=({img1,img2,img3})=>{
     return (
         <div className="pdImages">
-            <div className="pdAllImages">
+             <div className="pdAllImages">
                 <div className="sinleSmallImage"><img src={img1} alt="" /></div>
                 <div className="sinleSmallImage"><img src={img2} alt="" /></div>
                 <div className="sinleSmallImage"><img src={img3} alt="" /></div>
-            </div>
-            <div className="pdMainImage">
+             </div>
+             <div className="pdMainImage">
                 <div className="leftArrow"><KeyboardArrowLeftIcon/></div>
                 <div className="divMainImg"><img src={img1} alt="" /></div>
                 <div className="rightArrow"><KeyboardArrowRightIcon/></div>
-            </div>
+             </div>
         </div>
     )
 }
