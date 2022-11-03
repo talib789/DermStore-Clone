43bd713963ec5ad40df8b9be01cd58ab255cc41e
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-03 09:16:58 +0530

    All links are updated

--- client/src/Pages/Payment/CheckoutPage.jsx
+++ client/src/Pages/Payment/CheckoutPage.jsx
@@ -31,11 +31,11 @@ export const CheckoutPage = () => {
   // }, []);
   console.log(TotalPay);
   //   async function getData() {
-  //     const data = await fetch("https://ayush05.herokuapp.com/dermcart").then((d) =>
-  //       d.json()
+  //    const data = await fetch("https://ayush05.herokuapp.com/dermcart").then((d) =>
+  //      d.json()
   //     );
   //     setCartitem(data);
-  //     //  console.log(data);
+  //    //  console.log(data);
   //   }
   // const [paymentSucsess, setPaymentSucsesss] = React.useState(false);
   React.useEffect(() => {
@@ -48,12 +48,12 @@ export const CheckoutPage = () => {
       .then(({ data }) => {
         dispatch(fetchCartData(data));
         dispatch(GetCartCount(data[0].cartItems.length));
-        // console.log(data);
+         // console.log(data);
       });
   };
 
   function getCartData() {
-    // console.log(` https://dermstore-server-ayush.herokuapp.com/items/${Token}`);
+     // console.log(` https://dermstore-server-ayush.herokuapp.com/items/${Token}`);
     fetch(` https://dermstore-server-ayush.herokuapp.com/items/${Token}`)
       .then((res) => res.json())
       .then((res) => setCartdata(res[0].cartItems))
