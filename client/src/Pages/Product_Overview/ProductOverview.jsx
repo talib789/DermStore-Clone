e4d35d59c0dcb5b82a36467a6592380369bc54e6
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-12-25 10:42:42 +0530

    Api links Updated on Cyclic

--- client/src/Pages/Product_Overview/ProductOverview.jsx
+++ client/src/Pages/Product_Overview/ProductOverview.jsx
@@ -8,6 +8,7 @@ import { ItemCountCounter } from "./ItemCountCounter";
 
 import Rating from "@mui/material/Rating";
 import Stack from "@mui/material/Stack";
+
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -22,7 +23,7 @@ export const ProductOverview = () => {
   const [data, setData] = React.useState({});
 
   const getSearchData = () => {
-    fetch(`https://ayush05.herokuapp.com/dermproducts?q=${id}`)
+    fetch(`https://dermstoreproducts.cyclic.app/dermproducts?q=${id}`)
       .then((res) => res.json())
       .then((res) => setData(res[0]))
       .catch((error) => console.log(error));
@@ -37,9 +38,9 @@ export const ProductOverview = () => {
       <Navbar />
       <div>
         <div className="pd1">
-           <p className="pd2">
+          <p className="pd2">
             Home / Skincare / {data.item_category} / {data.name}
-           </p>
+          </p>
         </div>
 
         <div className="pd3">
@@ -78,8 +79,8 @@ export const ProductOverview = () => {
                   aria-labelledby="demo-radio-buttons-group-label"
                   defaultValue={data.mrp}
                   name="radio-buttons-group"
-                  >
-                   <div id="rateOption">
+                >
+                  <div id="rateOption">
                     <FormControlLabel
                       value={data.mrp}
                       control={<Radio />}
@@ -95,7 +96,7 @@ export const ProductOverview = () => {
                   </div>
                 </RadioGroup>
               </FormControl>
-             </div>
+            </div>
             <div className="mrpDiv">
               <h3>{"$" + data.mrp}</h3>
               <p>Subscribe to this product to save 15%</p>
